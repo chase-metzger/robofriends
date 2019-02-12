eaee736e0c93a3383a58067d44096d5757068963
Conrad Chase Metzger <cmetzger@bearcubs.santarosa.edu>
2019-02-11 21:40:20 -0800

    Updates

---
+++ precache-manifest.fe91c8b8e5a1f5c8250babe9a40d83db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5d85d34e3c1081d1ea0",
+    "url": "/robofriends/static/css/main.ffa5eca3.chunk.css"
+  },
+  {
+    "revision": "e5d85d34e3c1081d1ea0",
+    "url": "/robofriends/static/js/main.e5d85d34.chunk.js"
+  },
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "4410f654c93763af4659",
+    "url": "/robofriends/static/css/2.037d0a6f.chunk.css"
+  },
+  {
+    "revision": "4410f654c93763af4659",
+    "url": "/robofriends/static/js/2.4410f654.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f868e3365a53ec15c618160aa2d70e59",
+    "url": "/robofriends/index.html"
+  }
+];
