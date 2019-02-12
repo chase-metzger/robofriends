5412170a179251a76f67bd7a0aba40529ebfd5fc
Conrad Chase Metzger <cmetzger@bearcubs.santarosa.edu>
2019-02-11 18:44:06 -0800

    Refactor CounterButton to a stateful functional component

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,21 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-class CounterButton extends React.Component {
-  constructor () {
-    super()
-
-    this.state = {
-      count: 0
-    }
-  }
-
-  shouldComponentUpdate (nextProps, nextState) {
-    return this.state.count !== nextState.count
-  }
-
-  render () {
-    return <button color={this.props.color} onClick={() => this.setState(state => ({ count: state.count + 1 }))}>Count: {this.state.count}</button>
-  }
+export default function CounterButton ({ color }) {
+  const [count, setCount] = useState(0)
+  return <button color={color} onClick={() => setCount(count + 1)}>Count: {count}</button>
 }
-
-export default CounterButton
